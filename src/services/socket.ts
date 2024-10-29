114f5db1dc6bac85a9d6fef1358e6ecdff7f3da3
Anucha Kaewnil <anucha.k@cliniter.com>
2024-10-29 12:47:50 +0700

    auth

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,9 +1,14 @@
 import { io } from 'socket.io-client';
 
 export const initializeSocket = () => {
-    const socket = io('wss://api.artico.in.th', {
+    const url = 'wss://api.artico.in.th'
+    const url1 = 'ws://192.168.50.143:3000'
+    const socket = io(url, {
         transports: ['websocket'],
-        closeOnBeforeunload: false
+        closeOnBeforeunload: false,
+        auth: {
+            userId: '2c85ee2e-5b0f-41af-b6fe-5ce0b0dac643'
+        }
     });
 
     socket.on('connect', () => {
